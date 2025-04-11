749eb5a4cfbb09cf7764a7f10646ab662b248ff1
Dimitar-Peev <mctrix87@abv.bg>
2025-04-11 17:30:31 +0300

    Update "RoleService.java"

--- src/main/java/com/example/judge/service/RoleService.java
+++ src/main/java/com/example/judge/service/RoleService.java
@@ -1,6 +1,11 @@
 package com.example.judge.service;
 
+import com.example.judge.model.entity.Role;
+import com.example.judge.model.entity.RoleName;
+
 public interface RoleService {
 
     void init();
+
+    Role findRole(RoleName name);
 }
