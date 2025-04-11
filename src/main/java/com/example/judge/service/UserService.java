10bbb1b38e7fe3ff627c320f7ae44e881d6e26f8
Dimitar-Peev <mctrix87@abv.bg>
2025-04-11 17:29:25 +0300

    Add "UserService.java"

---
+++ src/main/java/com/example/judge/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.judge.service;
+
+import com.example.judge.model.service.UserServiceModel;
+
+public interface UserService {
+
+    boolean register(UserServiceModel userServiceModel);
+}
