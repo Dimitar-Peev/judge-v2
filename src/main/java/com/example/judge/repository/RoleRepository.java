663dc35c4a963ad47b94c899a0b2ad59f0d3faf1
Dimitar-Peev <mctrix87@abv.bg>
2025-04-12 11:40:25 +0300

    Add "RoleRepository.java"

---
+++ src/main/java/com/example/judge/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.example.judge.repository;
+
+import com.example.judge.model.entity.Role;
+import com.example.judge.model.entity.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, String> {
+
+    Optional<Role> findByName(RoleName name);
+}
