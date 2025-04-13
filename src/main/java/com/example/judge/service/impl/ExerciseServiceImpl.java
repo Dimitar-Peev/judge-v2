7405be484dfec1edafa6c7e6ec97d81b080a3e3a
Dimitar-Peev <mctrix87@abv.bg>
2025-04-13 13:03:29 +0300

    Add "ExerciseServiceImpl.java"

---
+++ src/main/java/com/example/judge/service/impl/ExerciseServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.judge.service.impl;
+
+import com.example.judge.model.entity.Exercise;
+import com.example.judge.model.service.ExerciseServiceModel;
+import com.example.judge.repository.ExerciseRepository;
+import com.example.judge.service.ExerciseService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Slf4j
+@Service
+public class ExerciseServiceImpl implements ExerciseService {
+
+    private final ExerciseRepository exerciseRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public void add(ExerciseServiceModel exerciseServiceModel) {
+
+        Exercise exercise = modelMapper.map(exerciseServiceModel, Exercise.class);
+        this.exerciseRepository.save(exercise);
+        log.info("Successfully added exercise.");
+
+    }
+
+
+}
+
