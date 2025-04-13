c5ab914d90acc9440451f47cbce7e1e1740d151e
Dimitar-Peev <mctrix87@abv.bg>
2025-04-13 13:02:18 +0300

    Add "ExerciseAddBindingModel.java"

---
+++ src/main/java/com/example/judge/model/binding/ExerciseAddBindingModel.java
@@ -0,0 +1,25 @@
+package com.example.judge.model.binding;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ExerciseAddBindingModel {
+
+    @Size(min = 2, message = "Exercise name must be at least 2 characters long")
+    private String name;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @PastOrPresent(message = "The date cannot be in the future")
+    private LocalDateTime startedOn;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @FutureOrPresent(message = "The date cannot be in the past")
+    private LocalDateTime dueDate;
+
+}
