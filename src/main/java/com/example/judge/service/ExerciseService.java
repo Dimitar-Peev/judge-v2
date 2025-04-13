a549103d9b4de08814971cdb709b010f097ad611
Dimitar-Peev <mctrix87@abv.bg>
2025-04-13 13:03:13 +0300

    Add "ExerciseService.java"

---
+++ src/main/java/com/example/judge/service/ExerciseService.java
@@ -0,0 +1,8 @@
+package com.example.judge.service;
+
+import com.example.judge.model.service.ExerciseServiceModel;
+
+public interface ExerciseService {
+
+    void add(ExerciseServiceModel exerciseServiceModel);
+}
