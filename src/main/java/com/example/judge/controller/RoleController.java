1e3755940aef22e5902090b15334a3b15216f1d5
Dimitar-Peev <mctrix87@abv.bg>
2025-04-18 10:45:35 +0300

    Add "RoleController.java"

---
+++ src/main/java/com/example/judge/controller/RoleController.java
@@ -0,0 +1,46 @@
+package com.example.judge.controller;
+
+import com.example.judge.model.binding.RoleAddBindingModel;
+import com.example.judge.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/roles")
+public class RoleController {
+
+    private final UserService userService;
+
+    @GetMapping("/add")
+    public String add(Model model, HttpSession session) {
+
+        String userId = session.getAttribute("user_id").toString();
+        List<String> allUsernames = this.userService.findAllUsernamesExceptCurrent(userId);
+        model.addAttribute("usernames", allUsernames);
+
+        return "role-add";
+    }
+
+    @PostMapping("/add")
+    public String addConfirm(@Valid @ModelAttribute("roleAddBindingModel") RoleAddBindingModel roleAddBindingModel,
+                             BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "role-add";
+        }
+
+        this.userService.addRoleToUser(roleAddBindingModel.getUsername(), roleAddBindingModel.getRole());
+        return "redirect:/";
+    }
+}
