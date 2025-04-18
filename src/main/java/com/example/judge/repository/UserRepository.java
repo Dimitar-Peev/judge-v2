d45e18c4960caab551e230399a5b366bca8f2a76
Dimitar-Peev <mctrix87@abv.bg>
2025-04-18 10:43:53 +0300

    Update "UserRepository.java"

--- src/main/java/com/example/judge/repository/UserRepository.java
+++ src/main/java/com/example/judge/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.example.judge.repository;
 
 import com.example.judge.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +15,7 @@ public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUsernameOrEmail(String username, String email);
 
     Optional<User> findByUsername(String username);
+
+    @Query("SELECT u.username FROM User u WHERE u.id <> :currentUserId")
+    List<String> findAllUsernamesExceptCurrent(@Param("currentUserId") String currentUserId);
 }
