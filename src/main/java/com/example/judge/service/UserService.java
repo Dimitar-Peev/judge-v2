084fb7badf8c0ab868389a7387363576a9b4f9fa
Dimitar-Peev <mctrix87@abv.bg>
2025-04-18 10:43:20 +0300

    Update "UserService.java"

--- src/main/java/com/example/judge/service/UserService.java
+++ src/main/java/com/example/judge/service/UserService.java
@@ -2,6 +2,8 @@ package com.example.judge.service;
 
 import com.example.judge.model.service.UserServiceModel;
 
+import java.util.List;
+
 public interface UserService {
 
     boolean register(UserServiceModel userServiceModel);
@@ -9,5 +11,9 @@ public interface UserService {
     boolean login(UserServiceModel userServiceModel);
 
     void logout();
+
+    List<String> findAllUsernamesExceptCurrent(String userId);
+
+    void addRoleToUser(String username, String role);
 
 }
