5d7594534ac9c9403f743a2cf17852208acf8579
Dimitar-Peev <mctrix87@abv.bg>
2025-04-19 11:59:41 +0300

    Add "HomeworkService.java"

---
+++ src/main/java/com/example/judge/service/HomeworkService.java
@@ -0,0 +1,9 @@
+package com.example.judge.service;
+
+import com.example.judge.model.service.HomeworkServiceModel;
+
+public interface HomeworkService {
+
+    void add(HomeworkServiceModel homeworkServiceModel);
+
+}
