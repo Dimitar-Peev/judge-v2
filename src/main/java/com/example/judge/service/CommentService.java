02bda38c653bc6a3afad9e2ed671f8411424fdfd
Dimitar-Peev <mctrix87@abv.bg>
2025-05-20 20:11:15 +0300

    Add "CommentService.java"

---
+++ src/main/java/com/example/judge/service/CommentService.java
@@ -0,0 +1,9 @@
+package com.example.judge.service;
+
+import com.example.judge.model.service.CommentServiceModel;
+
+public interface CommentService {
+
+    void add(CommentServiceModel commentServiceModel);
+
+}
