6821d7f24875b09ad18a4ffdb0e85ffe3286c001
Dimitar-Peev <mctrix87@abv.bg>
2025-05-20 20:17:05 +0300

    Updated "HomeworkServiceModel.java"

--- src/main/java/com/example/judge/model/service/HomeworkServiceModel.java
+++ src/main/java/com/example/judge/model/service/HomeworkServiceModel.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 @Data
 public class HomeworkServiceModel {
 
+    private String id;
     private LocalDateTime addedOn;
     private String gitAddress;
     private UserServiceModel author;
