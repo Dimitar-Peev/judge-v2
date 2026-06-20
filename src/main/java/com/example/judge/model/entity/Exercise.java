8a3a3c56dd5abf620632124fd24ea30efce9d7d4
Dimitar-Peev <mctrix87@abv.bg>
2026-06-20 18:50:38 +0300

    refactor: enhance Exercise entity with validation annotations and Lombok integration

--- src/main/java/com/example/judge/model/entity/Exercise.java
+++ src/main/java/com/example/judge/model/entity/Exercise.java
@@ -1,25 +1,37 @@
 package com.example.judge.model.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "exercises")
 public class Exercise  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
     private String id;
 
+    @NotBlank
+    @Size(min = 2, max = 255)
     @Column(nullable = false, unique = true)
     private String name;
 
+    @NotNull
     @Column(nullable = false)
     private LocalDateTime startedOn;
 
+    @NotNull
     @Column(nullable = false)
     private LocalDateTime dueDate;
 
