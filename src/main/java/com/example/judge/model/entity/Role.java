8bd6da43888f4dd15843e23c47d4552b03fd1b9d
Dimitar-Peev <mctrix87@abv.bg>
2026-06-20 18:49:42 +0300

    refactor: enhance Role entity with validation and constructor annotations

--- src/main/java/com/example/judge/model/entity/Role.java
+++ src/main/java/com/example/judge/model/entity/Role.java
@@ -1,17 +1,26 @@
 package com.example.judge.model.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "roles")
 public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
     private String id;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     private RoleName name;
 
