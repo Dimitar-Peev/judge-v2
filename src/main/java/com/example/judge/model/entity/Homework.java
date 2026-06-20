80cb2869184c21554508ae173a1c1aacb6a4e91b
Dimitar-Peev <mctrix87@abv.bg>
2026-06-20 18:53:11 +0300

    refactor: enhance Homework entity with validation annotations and Lombok integration

--- src/main/java/com/example/judge/model/entity/Homework.java
+++ src/main/java/com/example/judge/model/entity/Homework.java
@@ -1,36 +1,51 @@
 package com.example.judge.model.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "homeworks")
 public class Homework {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
     private String id;
 
+    @NotNull
     @Column(nullable = false)
     private LocalDateTime addedOn;
 
+    @NotBlank
+    @Size(max = 500)
     @Column(nullable = false, unique = true)
     private String gitAddress;
 
+    @NotNull
     @ManyToOne
-    @JoinColumn(name = "author_id")
+    @JoinColumn(name = "author_id", referencedColumnName = "id", nullable = false)
     private User author;
 
+    @NotNull
     @ManyToOne
-    @JoinColumn(name = "exercise_id")
+    @JoinColumn(name = "exercise_id", referencedColumnName = "id", nullable = false)
     private Exercise exercise;
 
-    @OneToMany(mappedBy = "homework", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "homework", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Set<Comment> comments = new HashSet<>();
 
 }
