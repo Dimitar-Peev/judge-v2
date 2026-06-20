5fe290b671d327b187e985284af39a682ccce997
Dimitar-Peev <mctrix87@abv.bg>
2026-06-20 18:47:42 +0300

    refactor: improve User entity validation and relationships

--- src/main/java/com/example/judge/model/entity/User.java
+++ src/main/java/com/example/judge/model/entity/User.java
@@ -1,36 +1,53 @@
 package com.example.judge.model.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
     private String id;
 
+    @NotBlank
+    @Size(min = 2)
     @Column(unique = true, nullable = false)
     private String username;
 
+    @NotBlank
+    @Size(min = 3)
     @Column(nullable = false)
     private String password;
 
+    @NotBlank
+    @Email
     @Column(unique = true, nullable = false)
     private String email;
 
+    @NotBlank
     @Column(unique = true, nullable = false)
     private String git;
 
+    @NotNull
     @ManyToOne
+    @JoinColumn(name = "role_id", referencedColumnName = "id", nullable = false)
     private Role role;
 
-    @OneToMany(mappedBy = "author", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "author", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Set<Homework> homeworks = new HashSet<>();
 
 }
