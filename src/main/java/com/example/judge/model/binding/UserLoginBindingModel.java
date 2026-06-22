b217e205dcbab8826782d8573bb340447d708f8a
Dimitar-Peev <mctrix87@abv.bg>
2026-06-22 19:20:29 +0300

    feat: Add validation annotations to UserLoginBindingModel fields

--- src/main/java/com/example/judge/model/binding/UserLoginBindingModel.java
+++ src/main/java/com/example/judge/model/binding/UserLoginBindingModel.java
@@ -1,14 +1,17 @@
 package com.example.judge.model.binding;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserLoginBindingModel {
 
+    @NotBlank(message = "Username cannot be empty")
     @Size(min = 2, message = "Username length must be minimum 2 characters!")
     private String username;
 
+    @NotBlank(message = "Password cannot be empty")
     @Size(min = 3, message = "Password length must be minimum 3 characters!")
     private String password;
 
