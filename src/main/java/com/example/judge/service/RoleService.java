0427ff0bf3ceaddd4a6c18af400d7569099ae8fa
Dimitar-Peev <mctrix87@abv.bg>
2026-06-22 21:34:02 +0300

    refactor: Update RoleService to use RoleServiceModel in findByName method for improved consistency

--- src/main/java/com/example/judge/service/RoleService.java
+++ src/main/java/com/example/judge/service/RoleService.java
@@ -1,11 +1,12 @@
 package com.example.judge.service;
 
-import com.example.judge.model.entity.Role;
 import com.example.judge.model.entity.RoleName;
+import com.example.judge.model.service.RoleServiceModel;
 
 public interface RoleService {
 
     void init();
 
-    Role findRole(RoleName name);
+    RoleServiceModel findByName(RoleName roleName);
+
 }
