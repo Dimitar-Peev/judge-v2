397108f7d2d6413acffd7de6dd557fd5e53ab79e
Dimitar-Peev <mctrix87@abv.bg>
2026-06-22 19:15:18 +0300

    feat: Add validation annotations for UserRegisterBindingModel fields

--- src/main/java/com/example/judge/model/binding/UserRegisterBindingModel.java
+++ src/main/java/com/example/judge/model/binding/UserRegisterBindingModel.java
@@ -9,18 +9,21 @@ import lombok.Data;
 @Data
 public class UserRegisterBindingModel {
 
+    @NotBlank(message = "Username cannot be empty")
     @Size(min = 2, message = "Username length must be minimum 2 characters!")
     private String username;
 
+    @NotBlank(message = "Password cannot be empty")
     @Size(min = 3, message = "Password length must be minimum 3 characters!")
     private String password;
 
     private String confirmPassword;
 
-    @NotBlank(message = "Enter valid email address")
+    @NotBlank(message = "Email cannot be empty")
     @Email(message = "Enter valid email address")
     private String email;
 
+    @NotBlank(message = "GitHub profile cannot be empty")
     @Pattern(regexp = "https:\\/\\/github\\.com\\/.+", message = "Enter valid github address")
     private String git;
 
