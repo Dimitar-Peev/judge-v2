27c6a7db264265ee422135a8bc48e9a4ca0bff7b
Dimitar-Peev <mctrix87@abv.bg>
2026-06-23 18:12:09 +0300

    feat: Add validation constraints to RoleAddBindingModel for username and role fields

--- src/main/java/com/example/judge/model/binding/RoleAddBindingModel.java
+++ src/main/java/com/example/judge/model/binding/RoleAddBindingModel.java
@@ -1,11 +1,15 @@
 package com.example.judge.model.binding;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RoleAddBindingModel {
 
+    @NotBlank(message = "Please, select a username")
     private String username;
+
+    @NotBlank(message = "Please, select a role")
     private String role;
 
 }
