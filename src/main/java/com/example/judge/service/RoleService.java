0d36df6ae5317a4b18c5f51352c2473ffab95fd9
Dimitar-Peev <mctrix87@abv.bg>
2026-06-23 18:09:15 +0300

    feat: Add method to fetch all role names in RoleService interface

--- src/main/java/com/example/judge/service/RoleService.java
+++ src/main/java/com/example/judge/service/RoleService.java
@@ -3,10 +3,14 @@ package com.example.judge.service;
 import com.example.judge.model.entity.RoleName;
 import com.example.judge.model.service.RoleServiceModel;
 
+import java.util.List;
+
 public interface RoleService {
 
     void init();
 
     RoleServiceModel findByName(RoleName roleName);
+
+    List<String> findAllRoleNames();
 
 }
