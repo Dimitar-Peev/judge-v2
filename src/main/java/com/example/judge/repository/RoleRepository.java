af29b7d9de4258f077c7cc5e41ddf25730debfe5
Dimitar-Peev <mctrix87@abv.bg>
2026-06-23 18:08:43 +0300

    feat: Add custom query to RoleRepository to fetch role names ordered alphabetically

--- src/main/java/com/example/judge/repository/RoleRepository.java
+++ src/main/java/com/example/judge/repository/RoleRepository.java
@@ -3,12 +3,18 @@ package com.example.judge.repository;
 import com.example.judge.model.entity.Role;
 import com.example.judge.model.entity.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, String> {
 
     Optional<Role> findByName(RoleName name);
+
+    @Query(value = "SELECT name FROM roles ORDER BY name", nativeQuery = true)
+    List<String> findAllRoleNames();
+
 }
