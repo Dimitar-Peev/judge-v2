1e59ef383dc4e472857915d74e3302c42320bc27
Dimitar-Peev <mctrix87@abv.bg>
2026-06-25 19:20:51 +0300

    refactor: Improve HomeworkAddBindingModel validation messages for clarity

--- src/main/java/com/example/judge/model/binding/HomeworkAddBindingModel.java
+++ src/main/java/com/example/judge/model/binding/HomeworkAddBindingModel.java
@@ -1,16 +1,16 @@
 package com.example.judge.model.binding;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class HomeworkAddBindingModel {
 
-    @Size(min = 2, message = "Exercise name length must be more than 2 characters")
+    @NotBlank(message = "Please, select an exercise")
     private String exercise;
 
-    @Pattern(regexp = "https:\\/\\/github\\.com\\/.+/.+", message = "Enter git address following this pattern")
+    @Pattern(regexp = "https://github\\.com/.+/.+", message = "Enter git address following this pattern: https://github.com/username/homework")
     private String gitAddress;
 
 }
