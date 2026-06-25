5d9d6df3bb58960947a22e9420ffd559c273c104
Dimitar-Peev <mctrix87@abv.bg>
2026-06-25 18:30:08 +0300

    feat: Add methods to ExerciseService for fetching exercise names and finding exercises by name

--- src/main/java/com/example/judge/service/ExerciseService.java
+++ src/main/java/com/example/judge/service/ExerciseService.java
@@ -2,8 +2,13 @@ package com.example.judge.service;
 
 import com.example.judge.model.service.ExerciseServiceModel;
 
+import java.util.List;
+
 public interface ExerciseService {
 
     ExerciseServiceModel add(ExerciseServiceModel exerciseServiceModel);
-    
+
+    List<String> findAllExerciseNames();
+
+    ExerciseServiceModel findByName(String exercise);
 }
