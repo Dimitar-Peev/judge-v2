16991d3354aac55c1886dbeea5de9f9562c544f5
Dimitar-Peev <mctrix87@abv.bg>
2026-06-25 18:29:40 +0300

    feat: Add methods to ExerciseRepository for fetching exercise names and finding exercises by name

--- src/main/java/com/example/judge/repository/ExerciseRepository.java
+++ src/main/java/com/example/judge/repository/ExerciseRepository.java
@@ -2,10 +2,18 @@ package com.example.judge.repository;
 
 import com.example.judge.model.entity.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ExerciseRepository extends JpaRepository<Exercise, String> {
 
+    @Query(value = "SELECT name FROM exercises ORDER BY started_on", nativeQuery = true)
+    List<String> findAllExerciseNames();
+
+    Optional<Exercise> findByName(String exercise);
 
 }
