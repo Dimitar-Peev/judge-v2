25a2b4f7d45c14aed5b94ea217b8702c9330782a
Dimitar-Peev <mctrix87@abv.bg>
2026-06-26 15:58:41 +0300

    feat: Extend HomeworkService with methods for fetching and updating homework data

--- src/main/java/com/example/judge/service/HomeworkService.java
+++ src/main/java/com/example/judge/service/HomeworkService.java
@@ -1,9 +1,14 @@
 package com.example.judge.service;
 
+import com.example.judge.model.service.ExerciseServiceModel;
 import com.example.judge.model.service.HomeworkServiceModel;
+import jakarta.servlet.http.HttpSession;
 
 public interface HomeworkService {
 
-    void add(HomeworkServiceModel homeworkServiceModel);
+    void add(HomeworkServiceModel homeworkServiceModel, ExerciseServiceModel exerciseServiceModel, HttpSession httpSession);
 
+    HomeworkServiceModel findHomeworksByScoring();
+
+    HomeworkServiceModel findById(String homeworkId);
 }
