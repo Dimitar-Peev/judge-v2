db98aaef01d451955f618fb8e2db9767c8212052
Dimitar-Peev <mctrix87@abv.bg>
2026-06-26 16:23:18 +0300

    feat: Inject UserService into HomeworkServiceImpl and enhance add method with user lookup and logging

--- src/main/java/com/example/judge/service/impl/HomeworkServiceImpl.java
+++ src/main/java/com/example/judge/service/impl/HomeworkServiceImpl.java
@@ -7,30 +7,36 @@ import com.example.judge.model.service.HomeworkServiceModel;
 import com.example.judge.model.service.UserServiceModel;
 import com.example.judge.repository.HomeworkRepository;
 import com.example.judge.service.HomeworkService;
+import com.example.judge.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class HomeworkServiceImpl implements HomeworkService {
 
     private final HomeworkRepository homeworkRepository;
     private final ModelMapper modelMapper;
+    private final UserService userService;
 
     @Override
     public void add(HomeworkServiceModel homeworkServiceModel, ExerciseServiceModel exerciseServiceModel, HttpSession httpSession) {
         homeworkServiceModel.setAddedOn(LocalDateTime.now());
         homeworkServiceModel.setExercise(exerciseServiceModel);
-        UserServiceModel user = (UserServiceModel) httpSession.getAttribute("user");
+        String userId = httpSession.getAttribute("user_id").toString();
+        UserServiceModel user = this.userService.findById(userId);
         homeworkServiceModel.setAuthor(user);
 
         Homework homework = this.modelMapper.map(homeworkServiceModel, Homework.class);
 
         this.homeworkRepository.save(homework);
+        log.info("Successfully added homework.");
     }
 
     @Override
