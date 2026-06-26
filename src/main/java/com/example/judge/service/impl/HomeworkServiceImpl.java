e88b65b08a8fb9f8eddc280a7b8e4139060ebdca
Dimitar-Peev <mctrix87@abv.bg>
2026-06-26 15:59:23 +0300

    feat: Enhance HomeworkService with methods for adding, fetching by scoring, and finding homework by ID

--- src/main/java/com/example/judge/service/impl/HomeworkServiceImpl.java
+++ src/main/java/com/example/judge/service/impl/HomeworkServiceImpl.java
@@ -1,12 +1,18 @@
 package com.example.judge.service.impl;
 
+import com.example.judge.exception.ResourceNotFoundException;
 import com.example.judge.model.entity.Homework;
+import com.example.judge.model.service.ExerciseServiceModel;
 import com.example.judge.model.service.HomeworkServiceModel;
+import com.example.judge.model.service.UserServiceModel;
 import com.example.judge.repository.HomeworkRepository;
 import com.example.judge.service.HomeworkService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @Service
@@ -16,8 +22,29 @@ public class HomeworkServiceImpl implements HomeworkService {
     private final ModelMapper modelMapper;
 
     @Override
-    public void add(HomeworkServiceModel homeworkServiceModel) {
-        this.homeworkRepository.saveAndFlush(this.modelMapper.map(homeworkServiceModel, Homework.class));
+    public void add(HomeworkServiceModel homeworkServiceModel, ExerciseServiceModel exerciseServiceModel, HttpSession httpSession) {
+        homeworkServiceModel.setAddedOn(LocalDateTime.now());
+        homeworkServiceModel.setExercise(exerciseServiceModel);
+        UserServiceModel user = (UserServiceModel) httpSession.getAttribute("user");
+        homeworkServiceModel.setAuthor(user);
+
+        Homework homework = this.modelMapper.map(homeworkServiceModel, Homework.class);
+
+        this.homeworkRepository.save(homework);
+    }
+
+    @Override
+    public HomeworkServiceModel findHomeworksByScoring() {
+        return this.homeworkRepository.findTopHomeworkByCommentsCount()
+                .map(homework -> modelMapper.map(homework, HomeworkServiceModel.class))
+                .orElseThrow(() -> new ResourceNotFoundException("Not found any homework with comments"));
+    }
+
+    @Override
+    public HomeworkServiceModel findById(String homeworkId) {
+        return this.homeworkRepository.findById(homeworkId)
+                .map(homework -> this.modelMapper.map(homework, HomeworkServiceModel.class))
+                .orElseThrow(() -> new ResourceNotFoundException("Not found homework with id: " + homeworkId));
     }
 
 }
