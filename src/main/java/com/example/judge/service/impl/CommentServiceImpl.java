fa0d0d40570739ab4b2a745b70222b22dcd89d14
Dimitar-Peev <mctrix87@abv.bg>
2026-06-27 13:27:15 +0300

    feat: Enhance CommentService add method with user and homework association via services and session

--- src/main/java/com/example/judge/service/impl/CommentServiceImpl.java
+++ src/main/java/com/example/judge/service/impl/CommentServiceImpl.java
@@ -2,8 +2,13 @@ package com.example.judge.service.impl;
 
 import com.example.judge.model.entity.Comment;
 import com.example.judge.model.service.CommentServiceModel;
+import com.example.judge.model.service.HomeworkServiceModel;
+import com.example.judge.model.service.UserServiceModel;
 import com.example.judge.repository.CommentRepository;
 import com.example.judge.service.CommentService;
+import com.example.judge.service.HomeworkService;
+import com.example.judge.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -14,12 +19,23 @@ public class CommentServiceImpl implements CommentService {
 
     private final CommentRepository commentRepository;
     private final ModelMapper modelMapper;
+    private final HomeworkService homeworkService;
+    private final UserService userService;
 
     @Override
-    public void add(CommentServiceModel commentServiceModel) {
+    public CommentServiceModel add(CommentServiceModel commentServiceModel, HttpSession httpSession) {
+        HomeworkServiceModel homeworkServiceModel = this.homeworkService.findById(commentServiceModel.getHomework().getId());
+        commentServiceModel.setHomework(homeworkServiceModel);
+
+        String userId = httpSession.getAttribute("user_id").toString();
+        UserServiceModel user = this.userService.findById(userId);
+        commentServiceModel.setAuthor(user);
+
         Comment comment = this.modelMapper.map(commentServiceModel, Comment.class);
-        comment.setId(null);
-        this.commentRepository.save(comment);
+
+        Comment saved = this.commentRepository.save(comment);
+
+        return this.modelMapper.map(saved, CommentServiceModel.class);
     }
 
-}
+ }
