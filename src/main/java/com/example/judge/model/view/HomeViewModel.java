8627fcf01e8d925c172291edcc22f44b536859b2
Dimitar-Peev <mctrix87@abv.bg>
2026-06-29 21:19:04 +0300

    feat: Add HomeViewModel with fields for user count and student list

---
+++ src/main/java/com/example/judge/model/view/HomeViewModel.java
@@ -0,0 +1,18 @@
+package com.example.judge.model.view;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class HomeViewModel {
+
+    private long totalUsersCount;
+
+    private List<String> students;
+
+}
